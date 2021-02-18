34f94da8e392849a4b614a61b37d31ce94ec454b
Ian Beauregard <ian.beauregard@gmail.com>
2021-02-18 18:47:02 -0500

    Rename set_blockchain to load_blockchain

--- src/blockchain.h
+++ src/blockchain.h
@@ -8,7 +8,7 @@ typedef struct s_blockchain {
     Node *head;
 } Blockchain;
 
-void set_blockchain(Node *nodes);
+void load_blockchain(Node *nodes);
 Node *get_node_from_id(unsigned int nid);
 void add_node(Node *node);
 void rmv_node(Node *node);
