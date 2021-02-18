3380068a0d45df2d6ffe4ffc4be6d3e04bae302d
Ian Beauregard <ian.beauregard@gmail.com>
2021-02-18 17:41:07 -0500

    Function to know is node is synced

--- src/node.h
+++ src/node.h
@@ -2,6 +2,7 @@
 #define NODE_H
 
 #include "block.h"
+#include <stdbool.h>
 
 typedef struct s_node {
     unsigned int id;
@@ -16,6 +17,7 @@ Node *new_node(unsigned int nid);
 Block *get_block_from_id(unsigned int bid, Node *node);
 void add_block(Block *block, Node *node);
 void rmv_block(Block *block, Node *node);
+bool node_is_synced(Node *node);
 void free_node(Node *node);
 
 #endif
