1361a6a9617ed3f5326d20900e368792ff4ca554
Ian Beauregard <ian.beauregard@gmail.com>
2021-02-19 10:00:58 -0500

    Put back num_nodes field in the Blockchain type (useful for prompt display)

--- src/blockchain.h
+++ src/blockchain.h
@@ -6,12 +6,14 @@
 
 typedef struct s_blockchain {
     Node *head;
+    size_t num_nodes;
 } Blockchain;
 
 void load_blockchain(Node *nodes);
 Node *get_node_from_id(unsigned int nid);
 void add_node(Node *node);
 void rmv_node(Node *node);
+size_t get_num_nodes();
 bool blockchain_is_synced();
 void sync_blockchain();
 
