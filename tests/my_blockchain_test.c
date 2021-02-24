ef7b356a89823ae16439a2628bcec3c4b516ac65
Jimmy <jimmy.b.mau@gmail.com>
2021-02-24 10:22:29 -0500

    Removed unnecessary test files from tests dir

--- tests/my_blockchain_test.c
+++ tests/my_blockchain_test.c
@@ -2,8 +2,6 @@
 
 int main()
 {
-	test_block();
-	test_node();
 	test_blockchain();
 
 	return(0);
--- tests/my_blockchain_test.h
+++ tests/my_blockchain_test.h
@@ -1,8 +1,6 @@
 #ifndef MY_BLOCKCHAIN_TEST_H
 #define MY_BLOCKCHAIN_TEST_H
 
-void test_block();
-void test_node();
 void test_blockchain();
 
 #endif
